f8eefce19973e0a385e06a6b3315e1bff0fb0b5d
Ilya Zhuravlev <whatever@xyz.is>
2024-10-15 00:41:43 -0600

    desc_exploit: automatically derive the secret

--- derive_secret.py
+++ derive_secret.py
@@ -1,24 +1,32 @@
 import sys
 import struct
 
-keybox = bytes.fromhex(sys.argv[1])
 
-magic_table = []
-cur = 0
-for line in range(2):
-    magic_table.append([])
-    for x in range(5):
-        magic_table[-1].append(struct.unpack("<I", keybox[cur:cur+4])[0])
-        cur += 4
+def derive_secret(keybox):
+    magic_table = []
+    cur = 0
+    for line in range(2):
+        magic_table.append([])
+        for x in range(5):
+            magic_table[-1].append(struct.unpack("<I", keybox[cur:cur+4])[0])
+            cur += 4
 
-line = 0
-xored_buffer = b""
-for idx in range(5):
-    if idx == 3 or idx == 4:
-        x = magic_table[line][idx]
-    else:
-        x = magic_table[line + 1][idx]
+    line = 0
+    xored_buffer = b""
+    for idx in range(5):
+        if idx == 3 or idx == 4:
+            x = magic_table[line][idx]
+        else:
+            x = magic_table[line + 1][idx]
 
-    xored_buffer += struct.pack("<I", (~x) & 0xFFFFFFFF)
+        xored_buffer += struct.pack("<I", (~x) & 0xFFFFFFFF)
 
-print(xored_buffer.hex())
+    return xored_buffer
+
+
+def main():
+    print(derive_secret(bytes.fromhex(sys.argv[1])).hex())
+
+
+if __name__ == "__main__":
+    main()
